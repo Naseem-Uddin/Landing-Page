05fec8b16c94c487755f41fb84ddc3e57992dd4c
Naz <50588254+Naseem-Uddin@users.noreply.github.com>
2024-11-23 14:08:08 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,7 @@
 		$body = $('body');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+		breakpoints({xlarge:   [ '1281px',  '1680px' ], large:    [ '981px',   '1280px' ], medium:   [ '737px',   '980px'  ], small:    [ '481px',   '736px'  ], xsmall:   [ '361px',   '480px'  ], xxsmall:  [ null, '360px'  ]});
 
 	// Play initial animations on page load.
 		$window.on('load', function() {
